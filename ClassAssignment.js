8063a5fc33d4d64c4e99d8d74f4d7936c19f13f2
Arthur L. Rozier IV <a.rozier4@gmail.com>
2017-07-29 15:16:52 -0400

    Nowhere NEAR done

---
+++ ClassAssignment.js
@@ -0,0 +1,32 @@
+// function whosBad(num1, num2){
+
+//     if (num2 > num1){
+//         console.log("true");
+//     }
+//     else if (num2 === num1){
+//         console.log("-1");
+//     }
+//     else{
+//         console.log("false");
+//     }
+
+// }
+
+// whosBad(5, 3);
+
+// ===========================================
+
+// function addAll(num){
+//     var numSUM = 0;
+
+//     for (var i = 0; i < (num + 1); ++i){
+//         numSUM += i;
+//         // console.log(numSUM);
+//     }
+//     console.log("the sum of all numbers is: " + numSUM);
+//     return numSUM;
+// }
+
+// addAll(10);
+
+// ===========================================
